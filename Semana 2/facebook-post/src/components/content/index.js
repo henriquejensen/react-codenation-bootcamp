55e3e14a39434e6ddcd9e747ebcfc63bbf5f2a84
Henrique Jensen <henriquejensen@hotmail.com>
2019-07-20 16:06:53 -0300

    init facebook post

---
+++ Semana 2/facebook-post/src/components/content/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Text from '../text';
+
+function Content() {
+  return (
+    <div>
+      <Text text="Meu Texto" />
+    </div>
+  );
+}
+
+export default Content;
