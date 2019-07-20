b656b42513f83bd61f36789f8d2604c30ae19cc9
Henrique Jensen <henriquejensen@hotmail.com>
2019-07-20 15:57:47 -0300

    adds number of clicks

--- Semana 2/react-state/src/App.js
+++ Semana 2/react-state/src/App.js
@@ -11,14 +11,15 @@ class App extends React.Component {
   state = {
     text: 'Número de clicks 0',
     title: 'Meu titulo',
-    numberOfClicks: 0
+    numberOfClicks: [0, 1,2,3,4]
   }
 
   handleClickButton() {
-    const numberOfClicks = this.state.numberOfClicks + 1;
+    const lastIndex = this.state.numberOfClicks.length - 1;
+    const lastValue = this.state.numberOfClicks[lastIndex];
+    
     this.setState({
-      text: `Número de clicks ${numberOfClicks}` ,
-      numberOfClicks: numberOfClicks
+      numberOfClicks: this.state.numberOfClicks.concat(lastValue + 1)
     });
   }
 
@@ -26,7 +27,9 @@ class App extends React.Component {
     return (
       <div>
 
-        <p>{this.state.numberOfClicks}</p>
+        {this.state.numberOfClicks.map(function(click) {
+          return <p>{`Número de clicks ${click}`}</p>
+        })}
 
         <div>
           <Text text={this.state.title} />
